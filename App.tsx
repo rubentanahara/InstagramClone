dc8572eb033a61a177b4d3d05cd35d8637fa29f3
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-10-10 21:25:51 -0600

    clean

--- App.tsx
+++ App.tsx
@@ -1,24 +1,20 @@
-import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import Post from './src/components/Post/Post';
 import post from './src/assets/data/post';
-import {IPost} from './src/types/interfaces';
-interface IAppProps {
-  post: IPost;
-}
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.app}>
-      <ScrollView>
-        <Post post={post} />
-      </ScrollView>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.app}>
+            <ScrollView>
+                <Post post={post} />
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  app: {
-    flex: 1,
-  },
+    app: {
+        flex: 1,
+    },
 });
 export default App;
