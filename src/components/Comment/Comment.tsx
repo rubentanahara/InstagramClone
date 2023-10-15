a0e54a0a66b45aab468fc74d3bf706d264b3ed2c
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-10-14 18:05:49 -0600

    format

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,25 +1,26 @@
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import styles from './styles';
 import colors from '../../assets/theme/colors';
-import {IComment} from '../../types/interfaces';
+import { IComment } from '../../types/interfaces';
 
 interface ICommentProps {
-  comment: IComment;
+    comment: IComment;
 }
 
-const Comment = ({comment}: ICommentProps) => {
-  return (
-    <>
-      <View style={styles.comment}>
-        <Text style={styles.commentText}>
-          <Text style={styles.bold}>{comment.user.username} </Text>
-          {comment.comment}
-        </Text>
-        <AntDesign name={'hearto'} style={styles.icon} color={colors.black} />
-      </View>
-    </>
-  );
+const Comment = ({ comment }: ICommentProps) => {
+    const hey = '';
+    return (
+        <>
+            <View style={styles.comment}>
+                <Text style={styles.commentText}>
+                    <Text style={styles.bold}>{comment.user.username} </Text>
+                    {comment.comment}
+                </Text>
+                <AntDesign name={'hearto'} style={styles.icon} color={colors.black} />
+            </View>
+        </>
+    );
 };
 
 export default Comment;
