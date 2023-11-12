7a45f59b4e546268f8ffa5699993b0cde4e49c38
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-11-12 10:30:16 -0600

    refactor HomeScreen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,32 +1,28 @@
-import React, {useState, useRef} from 'react';
-import {FlatList, ViewabilityConfig, ViewToken} from 'react-native';
+import React from 'react';
+import {FlatList, ListRenderItemInfo} from 'react-native';
 import Post from '../../components/Post/Post';
 import posts from '../../assets/data/post.json';
+import useViewablePost from './hooks/useViewablePost'; // Custom hook for viewability logic
+
+interface PostItem {
+  id: string;
+}
 
 const HomeScreen = () => {
-  const [activePostId, setActivePostId] = useState<string | null>(null);
+  const {activePostId, onViewableItemsChanged} = useViewablePost();
 
-  const viewabilityConfig: ViewabilityConfig = {
-    itemVisiblePercentThreshold: 70,
-  };
-
-  const onViewableItemsChanged = useRef(
-    ({viewableItems}: {viewableItems: Array<ViewToken>}) => {
-      if (viewableItems.length > 0 && viewableItems[0].isViewable) {
-        setActivePostId(viewableItems[0].item.id);
-      }
-    },
+  const renderPost = ({item}: ListRenderItemInfo<PostItem>) => (
+    <Post post={item} isVisible={activePostId === item.id} />
   );
+
   return (
     <FlatList
       data={posts}
-      renderItem={({item}) => (
-        <Post post={item} isVisible={activePostId === item.id} />
-      )}
-      keyExtractor={item => item.id.toString()}
+      renderItem={renderPost}
+      keyExtractor={item => item.id}
       showsVerticalScrollIndicator={false}
-      viewabilityConfig={viewabilityConfig}
-      onViewableItemsChanged={onViewableItemsChanged.current}
+      viewabilityConfig={{itemVisiblePercentThreshold: 70}}
+      onViewableItemsChanged={onViewableItemsChanged}
     />
   );
 };
---
+++ src/screens/HomeScreen/hooks/useViewablePost.tsx
@@ -0,0 +1,18 @@
+import {useState, useRef} from 'react';
+import {ViewToken} from 'react-native';
+
+const useViewablePost = () => {
+  const [activePostId, setActivePostId] = useState<string | null>(null);
+
+  const onViewableItemsChanged = useRef(
+    ({viewableItems}: {viewableItems: Array<ViewToken>}) => {
+      if (viewableItems.length > 0 && viewableItems[0].isViewable) {
+        setActivePostId(viewableItems[0].item.id);
+      }
+    },
+  );
+
+  return {activePostId, onViewableItemsChanged: onViewableItemsChanged.current};
+};
+
+export default useViewablePost;
