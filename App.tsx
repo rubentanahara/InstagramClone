b760cc901642136323f698586fad3acc3b8c1039
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-11-12 10:11:38 -0600

    refactor App.tsx

--- App.tsx
+++ App.tsx
@@ -1,25 +1,16 @@
-import {PortalProvider} from '@gorhom/portal';
-import {StyleSheet} from 'react-native';
+import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {PortalProvider} from '@gorhom/portal';
+
 import EditProfile from './src/screens/EditProfile';
-import HomeScreen from './src/screens/HomeScreen/HomeScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
+import styles from './styles';
 
-const App = () => {
-  return (
-    <GestureHandlerRootView style={styles.app}>
-      <PortalProvider>
-        <EditProfile />
-      </PortalProvider>
-    </GestureHandlerRootView>
-  );
-};
+const App = () => (
+  <GestureHandlerRootView style={styles.app}>
+    <PortalProvider>
+      <EditProfile />
+    </PortalProvider>
+  </GestureHandlerRootView>
+);
 
-const styles = StyleSheet.create({
-  app: {
-    marginTop: '20%',
-    marginBottom: '10%',
-    flex: 1,
-  },
-});
 export default App;
---
+++ styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  app: {
+    marginTop: '20%',
+    marginBottom: '10%',
+    flex: 1,
+  },
+});
